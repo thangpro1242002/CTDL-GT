2c44213dc6de5617753719bba6544af5fa44a96c
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-07-08 09:54:27 +0700

    Add files via upload

---
+++ CTDLGT_Python/BT01ThapHaNoi.py
@@ -0,0 +1,10 @@
+n = int(input("Nhập số đĩa cần chuyển:"))
+def thapHaNoi(n, toaMot, toaHai, toaBa):
+
+    if n == 1:
+        print("Chuyen tu", toaMot, "sang", toaBa)
+    else:
+        thapHaNoi(n-1,toaMot,toaBa ,toaHai)
+        print("Chuyen tu", toaMot, "sang", toaBa)
+        thapHaNoi(n-1,toaHai,toaMot, toaBa)
+thapHaNoi(n,'A','B','C')
