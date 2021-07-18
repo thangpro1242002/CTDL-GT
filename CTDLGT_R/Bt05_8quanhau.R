4b51d20fc579dac158f106107312875b2d178b6a
thangpro1242002 <73933451+thangpro1242002@users.noreply.github.com>
2021-07-18 08:56:56 +0700

    Update Bt05_8quanhau.R

--- CTDLGT_R/Bt05_8quanhau.R
+++ CTDLGT_R/Bt05_8quanhau.R
@@ -1,25 +1,63 @@
 library(tidyverse)
-place_queen <- function(queens=c()) {
-  # If there are 8 queens placed, then this must be a solution.
+library(ggplot2)
+
+#-----------------------------------------------------------------------------
+#' Đặt một quân hậu cho trước một vectơ vị trí của các quân hậu hiện tại
+#'
+#' Hàm này tự gọi đệ quy cho mọi vị trí hợp lệ của quân hậu tiếp theo
+#'
+#' Một vectơ số nguyên đại diện cho vị trí cột của các quân hậu cho đến nay.
+#' Chỉ mục trong danh sách này là hàng và giá trị là cột.
+#' Để tạo tất cả các giải pháp, hãy chuyển vào một vectơ trống (mặc định)
+#'
+#'  Ví dụ quân hậu = c(1, 4, 7) tương ứng với các quân hậu được đặt tại c(1, 1), c(2, 4) và c(3, 7)
+#'                  
+#'
+#'    (Cột)   
+#'      8  |   |   |   |   |   |   |   |   |
+#'      | ---------------------------------
+#'      7  |   |   |   |   |   |   |   |   |
+#'      | ---------------------------------
+#'      6  |   |   |   |   |   |   |   |   |
+#'      | ---------------------------------
+#'      5  |   |   |   |   |   |   |   |   |
+#'      | ---------------------------------
+#'      4  |   |   |   |   |   |   |   |   |
+#'      | ---------------------------------
+#'      3  |   |   |   |   |   |   | Q |   |    Hàng thứ 3, cột thứ 7
+#'      | --------------------------------- 
+#'      2  |   |   |   | Q |   |   |   |   |    Hàng thứ 2, cột thứ 4
+#'      | ---------------------------------
+#'      1  | Q |   |   |   |   |   |   |   |    Hàng thứ 1, cột thứ 1
+#'      | 
+#'      0____1___2___3___4___5___6___7___8 (Hàng)
+#'
+#' Kết quả trả về danh sách trong đó mỗi phần tử là một vectơ gồm 8 số nguyên chính là một giải pháp cho bài toán 8 quân hậu
+#' 
+#-----------------------------------------------------------------------------
+
+place_queen <- function(queens=c()) { 
+  
+  # Nếu có 8 quân hậu được đặt, thì đây phải là một giải pháp.
   if (length(queens) == 8) {
     return(list(queens))
   }
   
-  # Figre out where a queen can be placed in the next row.
-  # Drop all columns that have already been taken - since we 
-  # can't place a queen below an existing queen
+  # Tìm ra nơi quân hậu có thể được đặt ở hàng tiếp theo.
+  # Bỏ tất cả các cột đã được lấy
+  # vì không thể đặt một quân hậu bên dưới một quân hậu hiện có
   possible_placements <- setdiff(1:8, queens)
   
-  # For each queen already on the board, find the diagonal 
-  # positions that it can see in this row.
+  # Đối với mỗi quân hậu đã có trên bàn cờ
+  # Hãy tìm các vị trí đường chéo mà nó có thể nhìn thấy trong hàng này.
   diag_offsets <- seq.int(length(queens), 1)
   diags <- c(queens + diag_offsets, queens - diag_offsets)
   diags <- diags[diags > 0 & diags < 9]
   
-  # Drop these diagonal columns from possible placements
+  # Thả các cột đường chéo này khỏi các vị trí có thể có
   possible_placements <- setdiff(possible_placements, diags)
   
-  # For each possible placement, try and place a queen
+  # Đối với mỗi vị trí có thể, hãy thử và đặt một quân hậu
   possible_placements %>% 
     map(~place_queen(c(queens, .x))) %>%
     keep(~length(.x) > 0) %>%
@@ -27,33 +65,38 @@ place_queen <- function(queens=c()) {
 }
 
 
-#-----------------------------------------------------------------------------
-#' Plot a single solution
-#' @param queens a vector of 8 integers giving the column positions of 8 queens
-#-----------------------------------------------------------------------------
-plot_single_8queens <- function(queens) {
-  queens_df <- data_frame(cols = queens, rows=1:8)
+# Lập một giải pháp duy nhất
+# Một vectơ gồm 8 số nguyên cho vị trí cột của 8 hoàng hậu
+plot_single_8queens <- function(queens, title = NULL) {
+  queens_df <- tibble(cols = queens, rows=1:8)
   board_df <- expand.grid(cols = 1:8, rows = 1:8) %>% 
     mutate(check = (cols + rows) %%2 == 1)
   
-  ggplot(queens_df, aes(cols, rows)) + 
+  p <- ggplot(queens_df, aes(rows, cols)) + 
     geom_tile(data=board_df, aes(fill=check), colour='black') +
-    geom_label(label='Q', fill='lightblue') + 
+    geom_text(label='\u2655', family="Arial Unicode MS", size = 8) + 
     theme_void() + 
     coord_equal() + 
-    scale_fill_manual(values = c('TRUE'='white', 'FALSE'='black')) + 
+    scale_fill_manual(values = c('TRUE'='white', 'FALSE'='grey70')) + 
     theme(
       legend.position = 'none'
-    ) + 
-    labs(title = paste("Queens", deparse(as.numeric(queens))))
+    ) 
   
+  if (is.null(title)) {
+    p <- p + labs(title = paste("Queens:", deparse(as.numeric(queens)))) 
+  } else {
+    p <- p + labs(title = title)
+  }
 }
 
 
-#-----------------------------------------------------------------------------
-# Start with no queens placed and generate all solutions. Plot the first 2
-#-----------------------------------------------------------------------------
+
+# Bắt đầu với không có quân hậu nào được đặt và tạo ra tất cả các giải pháp.
 solutions <- place_queen()
-length(solutions)
-plot_single_8queens(solutions[[1]])
-plot_single_8queens(solutions[[2]]
+v=1:8
+f=function(q){L=length(q)
+if(L==8){q}else{flatten(map(setdiff(v,c(q,q+L:1,q-L:1)),~f(c(q,.))))}}
+s=data.frame(c=unlist(f(c())),r=v,x=rep(1:92,e=8),z=3)
+b=mutate(crossing(c=v,r=v),z=(c+r)%%2)
+g=geom_tile
+ggplot(s,aes(r,c,fill=z))+g(d=b)+g()+facet_wrap(~x)
